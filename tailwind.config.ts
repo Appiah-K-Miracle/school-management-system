78630c1108b46484728371b0ef9c409cae91ea78
Appiah-K-Miracle <amdwatkins123@gmail.com>
2024-10-08 07:36:18 +0000

    add some colors and small change

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,11 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primarySky: "#C3EBFA",
+        secondarySkyLight: "#EDF9FD",
+        tertiaryPurple: "#CFCEFF",
+        PurpleLight: "#FAE27C",
+        YellowLight: "#FEFCE8",
       },
     },
   },
